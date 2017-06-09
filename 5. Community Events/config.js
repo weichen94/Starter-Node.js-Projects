f3f4d4ae6a5dde573116933a72d4cb877314b755
Xu Weichen <weichen94@gmail.com>
2017-06-09 15:20:16 +0800

    Add files via upload

---
+++ 5. Community Events/config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+exports.port = process.env.PORT || 3000;
+exports.mongodb = {
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://weichen:password@ds115752.mlab.com:15752/comeventsapp'
+};
+exports.companyName = 'Orbital Inc.';
+exports.projectName = 'Math PYP';
+exports.systemEmail = 'weichenproject@gmail.com';
+exports.cryptoKey = 'k3yb0ardc4t';
+exports.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
+};
+exports.requireAccountVerification = false;
+exports.smtp = {
+  from: {
+    name: process.env.SMTP_FROM_NAME || exports.projectName +' ComEvents',
+    address: process.env.SMTP_FROM_ADDRESS || 'weichenproject@gmail.com'
+  },
+  credentials: {
+    user: process.env.SMTP_USERNAME || 'weichenproject@gmail.com',
+    password: process.env.SMTP_PASSWORD || 'xirvd27982',
+    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    ssl: true
+  }
+};
+exports.oauth = {
+  twitter: {
+    key: process.env.TWITTER_OAUTH_KEY || '',
+    secret: process.env.TWITTER_OAUTH_SECRET || ''
+  },
+  facebook: {
+    key: process.env.FACEBOOK_OAUTH_KEY || '',
+    secret: process.env.FACEBOOK_OAUTH_SECRET || ''
+  },
+  github: {
+    key: process.env.GITHUB_OAUTH_KEY || '',
+    secret: process.env.GITHUB_OAUTH_SECRET || ''
+  },
+  google: {
+    key: process.env.GOOGLE_OAUTH_KEY || '',
+    secret: process.env.GOOGLE_OAUTH_SECRET || ''
+  },
+  tumblr: {
+    key: process.env.TUMBLR_OAUTH_KEY || '',
+    secret: process.env.TUMBLR_OAUTH_SECRET || ''
+  }
+};
